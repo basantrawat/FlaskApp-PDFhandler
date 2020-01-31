5faaaa692351752cec4aa3774396873e5bf3e79c
Basant <basantsrwt@gmail.com>
2020-01-31 18:55:40 +0530

    Version 1.0.0

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request
+import PyPDF2
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/pdfMerger', methods=['GET', 'POST'])
+def pdfMerger():   
+    pdfs = ['']
+    if(request.method=='POST'):
+        pdfs = request.form.getlist('filename')
+        if pdfs==['']:
+            file_path=""
+            return render_template('pdfMerger.html', msg="Choose Some Files First", pdfFile=file_path)
+        else:
+            msg=""
+            pdfMerger = PyPDF2.PdfFileMerger()      
+            for pdf in pdfs: 
+                pdfMerger.append('pdfFiles/'+pdf) 
+                pdfMerger.write('mergedPDF/mergedPDFOutput.pdf')
+            file_path = 'mergedPDF/mergedPDFOutput.pdf'
+            return render_template('pdfMerger.html', pdfFile=file_path, msg=msg)
+    
+    else:
+        return render_template('pdfMerger.html', pdfFile="")
+
+
+
+@app.route('/pdfMergerOrdered', methods=['GET', 'POST'])
+def pdfMergerOrdered():   
+    if(request.method=='POST'):
+        pdf1 = request.form.get('filename1')
+        pdf2 = request.form.get('filename2')
+        pdf3 = request.form.get('filename3')
+        pdf4 = request.form.get('filename4')
+        
+        pdfs = [pdf1,pdf2,pdf3,pdf4]
+        pdfMerger = PyPDF2.PdfFileMerger() 
+
+        if pdfs==['', '', '', '']:
+            file_path=""
+            return render_template('pdfMergerOrdered.html', msg="Choose Some Files First", pdfFile=file_path)
+        else:
+            for pdf in pdfs: 
+                if pdf!='':
+                    pdfMerger.append('pdfFiles/'+pdf) 
+                    pdfMerger.write('mergedPDF/mergedPDFOutput.pdf')
+            
+            file_path = 'mergedPDF/mergedPDFOutput.pdf'
+            return render_template('pdfMergerOrdered.html', pdfFile=file_path, msg='')
+
+    else:
+        return render_template('pdfMergerOrdered.html', pdfFile='')
+
+
+
+@app.route('/pdfToText', methods=['GET', 'POST'])
+def pdfToText():   
+    if(request.method=='POST'):
+        pdffile = request.form.get('filename')
+        # creating a pdfFile object 
+        pdfFileObj = open(f'pdfFiles/{pdffile}', 'rb') 
+        # creating a pdfReader object 
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+        # Printing Number of pages in pdf file
+        pages = pdfReader.numPages
+
+        ConvertedTextData = ''
+        for i in range(pages):
+            # creating a page object 
+            pageObj = pdfReader.getPage(i)
+            # extracting text from page 
+            content = pageObj.extractText()
+            ConvertedTextData = ConvertedTextData + content
+            ConvertedTextData = ConvertedTextData + '\n'
+
+        pdfFileObj.close()
+        return render_template('pdfToText.html', convertedData=ConvertedTextData)
+    else:
+        return render_template('pdfToText.html')
+        
+app.run(debug=True)
